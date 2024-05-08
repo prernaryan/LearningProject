0a42e7496ac6ba3fc78ac23ad4929ae494a56ff4
prernaryan <prernasharma62333@gmail.com>
2024-05-08 16:18:22 +0530

    'Icons Done'

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,28 @@
-import {View, Text} from 'react-native';
+import {View} from 'react-native';
 import React from 'react';
 import FlashMessage from 'react-native-flash-message';
 import {GlobalStyleValues} from './constants';
 import {NetworkProvider} from './contextApi/NetworkContext';
 import ToastBanner from '@components/reuseableComponents/toastBanner';
+import {AppText} from '@components/reuseableComponents/appText';
+import {StatusBar} from 'native-base';
 
 const App = (): React.JSX.Element => {
   return (
-    <View>
+    <View style={{marginTop: 70}}>
       <NetworkProvider>
-        <Text>App</Text>
+        <StatusBar
+          translucent
+          backgroundColor={GlobalStyleValues.TransParent}
+          barStyle={GlobalStyleValues.DARK_CONTENT}
+        />
         <FlashMessage
           position={GlobalStyleValues.TOP}
           //@ts-ignore
           renderCustomContent={options => <ToastBanner options={options} />}
         />
+
+        <AppText>App</AppText>
       </NetworkProvider>
     </View>
   );
