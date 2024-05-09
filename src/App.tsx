894b7f20905a2861475c65cb171f7b03829ce7b0
prernaryan <prernasharma62333@gmail.com>
2024-05-09 14:49:42 +0530

    'changed Icon'

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,18 @@
-import {View} from 'react-native';
-import React from 'react';
+import {Platform, View} from 'react-native';
+import React, {useEffect} from 'react';
 import FlashMessage from 'react-native-flash-message';
 import {GlobalStyleValues} from './constants';
 import {NetworkProvider} from './contextApi/NetworkContext';
 import ToastBanner from '@components/reuseableComponents/toastBanner';
 import {AppText} from '@components/reuseableComponents/appText';
 import {StatusBar} from 'native-base';
+import SplashScreen from 'react-native-splash-screen';
 
 const App = (): React.JSX.Element => {
+  useEffect(() => {
+    SplashScreen.hide();
+  }, []);
+
   return (
     <View style={{marginTop: 70}}>
       <NetworkProvider>
